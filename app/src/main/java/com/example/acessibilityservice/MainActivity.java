4d51deca34872ac238183371ae5b354dc0e3b678
Parsy_Desktop <dumitru.tronciu@gmail.com>
2024-01-04 17:32:33 +0200

    First commit

--- app/src/main/java/com/example/acessibilityservice/MainActivity.java
+++ app/src/main/java/com/example/acessibilityservice/MainActivity.java
@@ -5,20 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Button allowPermission, adguard;
+    private TextView serviceStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        serviceStatus = findViewById(R.id.serviceStatus);
 
         allowPermission = findViewById(R.id.allowPermission);
         allowPermission.setOnClickListener(new View.OnClickListener() {
@@ -33,14 +38,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = getPackageManager().getLaunchIntentForPackage("com.android.adguard");
-                try {
-                    intent.setAction(Intent.ACTION_SEND);
-                    intent.putExtra("ACTION", 2);
-                    startActivity(intent);
-                    // TODO: Start protection with/without app
-
-                }catch(ActivityNotFoundException e){
-                    e.printStackTrace();
+                if (intent == null) { // Package not found
                     // Send user to AdGuard market page
                     intent = new Intent(Intent.ACTION_VIEW);
                     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -48,7 +46,23 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                 }
 
+                intent.setAction(Intent.ACTION_SEND);
+                intent.putExtra("ACTION", 2);
+                startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (MyAccessibilityService.isRunning()){
+            serviceStatus.setText("Running");
+            serviceStatus.setTextColor(Color.parseColor("0x008000"));   // Green
+        }else{
+            serviceStatus.setText("Stoped");
+            serviceStatus.setTextColor(Color.parseColor("0xFF0000"));   // Red
+        }
+
     }
 }
--- app/src/main/java/com/example/acessibilityservice/MyAccessibilityService.java
+++ app/src/main/java/com/example/acessibilityservice/MyAccessibilityService.java
@@ -2,47 +2,66 @@ package com.example.acessibilityservice;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.ActivityManager;
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
+
+import java.util.List;
 
 // TODO: Run service independently in background (bound or run in a separate thread or in foreground?)
 public class MyAccessibilityService extends AccessibilityService {
     private static final String TAG = "MyAccessibilityService";
+    private static boolean isRunning = false;
+    private boolean isAdGuardRunning = false;
+
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
-        Log.e(TAG, "onAccessibilityEvent: " );
+        Log.e(TAG, "onAccessibilityEvent: ");
         String packageName = event.getPackageName().toString();
-        PackageManager packageManager = this.getPackageManager();
-        try {
-            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(packageName, 0);
-            CharSequence applicationLabel =  packageManager.getApplicationLabel(applicationInfo);
-            Log.e(TAG, "app name is : " + applicationInfo.packageName );
-        } catch (PackageManager.NameNotFoundException e) {
-            throw new RuntimeException(e);
+        if (packageName.equals("com.google.android.googlequicksearchbox") && !isAdGuardRunning) {
+            startAdGuard();
+            isAdGuardRunning = true;
         }
+        Log.e(TAG, "app name is : " + packageName);
 
     }
 
     @Override
     public void onInterrupt() {
-        Log.e(TAG, "onInterrupt: something went wrong" );
+        Log.e(TAG, "onInterrupt: something went wrong");
     }
 
     @Override
     protected void onServiceConnected() {
         super.onServiceConnected();
-
-        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
-        info.eventTypes = AccessibilityEvent.TYPE_VIEW_CLICKED |
-                AccessibilityEvent.TYPE_VIEW_FOCUSED;
-
-        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_SPOKEN;
-
-        info.notificationTimeout = 100;
-
-        this.setServiceInfo(info);
-        Log.e(TAG, "onServiceConnected: " );
+        isRunning = true;
+        Log.e(TAG, "onServiceConnected: ");
     }
+
+    public static boolean isRunning() {
+        return isRunning;
+    }
+
+    private void startAdGuard() {
+        Intent intent = this.getPackageManager().getLaunchIntentForPackage("com.adguard.android");
+        if (intent == null) {
+            Log.e(TAG, "startAdGuard: Package not found");
+            // Send user to AdGuard market page
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse("market://details?id=" + "com.adguard.android"));
+            startActivity(intent);
+            return;
+        }
+        // Start AdGuard
+        intent.setAction(Intent.ACTION_SEND);
+        intent.putExtra("ACTION", 2);
+        startActivity(intent);
+        Log.e(TAG, "startAdGuard: Activity started");
+    }
+
 }
